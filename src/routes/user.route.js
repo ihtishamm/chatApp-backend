d438abaff1af504258ffbfb5b307248dc41af4af
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-17 02:09:16 +0500

    addd the routes for the updateAvatar, current user, refreshAccesstoken apis

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, RefreshAccessToken, getCurrentUser, updateUserAvatar } from "../controllers/user.controller.js";
  import { upload } from "../middlewares/multer.middleware.js";
+ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router(); 
 
  router.route("/register").post(upload.single("avatar"),registerUser);
  router.route("/login").post(loginUser)
+ 
+   // authenticated routes
+ router.route("/logout").post(verifyJWT,logoutUser)
+ router.route("/refreshToken").post(verifyJWT,RefreshAccessToken)
+ router.route("/current").get(verifyJWT,getCurrentUser)
+ router.route("/updateAvatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
 export default router;
