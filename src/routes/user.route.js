49c6917e0ae7f81cdff9932930d30985ae52afb1
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-16 19:13:15 +0500

    set the user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getUser } from "../controllers/user.controller.js";
-
+import { registerUser } from "../controllers/user.controller.js";
+ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router(); 
 
- router.route("/").get(getUser);
+ router.route("/register").post( upload.single("avatar"),registerUser);
 
 
 export default router;
