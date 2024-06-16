e3d0245e04d7ca0e20a0a3f3fa3246e9d3ee6d16
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-16 14:00:19 +0500

    message schema

---
+++ src/models/message.model.js
@@ -0,0 +1,29 @@
+import  mongoose,{ Schema, Types } from "mongoose";
+
+ const MessageSchema = new Schema({
+    
+     sender: {
+            type: Types.ObjectId,
+            ref:"User",
+            required: true,
+        },
+        chat:{
+            type: Types.ObjectId,
+            ref:"Chat",
+            required: true,
+        },
+        content:{
+            type: String,
+            required: true,
+        },
+        attachments:{
+            type: String,
+            required: true
+        }
+    
+     
+
+ },{timestamps:true});
+
+
+export const Message = mongoose.model("Message", MessageSchema);
