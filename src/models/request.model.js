c8a726fcd3c35816fedcf698a876cd6b483dd9b1
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-16 14:00:27 +0500

    request schema

---
+++ src/models/request.model.js
@@ -0,0 +1,23 @@
+import  mongoose,{ Schema, Types } from "mongoose";
+
+ const RequestSchema = new Schema({
+   
+      status:{
+        type:String,
+        default:"pending",
+        enum:["pending","accepted","rejected"],
+      },
+        sender:{
+            type: Types.ObjectId,
+            ref:"User",
+            required: true,
+        },
+        reciever:{
+            type: Types.ObjectId,
+            ref:"User",
+            required: true,
+        },
+ },{timestamps:true});
+
+
+export const Request = mongoose.model("Request", RequestSchema);
