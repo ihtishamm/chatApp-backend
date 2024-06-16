adc3912f91273c0fac55079e3be6d1e82fee635d
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-16 12:28:27 +0500

    connect with the mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-  
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running at port:  ${process.env.PORT}`)
+    })
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+     })
+})
+.catch((err) => {
+    console.log("Mongodb connection faild !!!",err)
+})
+
+
