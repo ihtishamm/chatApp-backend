10ac30c1ac3bf00ba8d02aa8a53b9c64b595b1c7
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-17 14:43:58 +0500

    write chat route and fix issues

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
  import { verifyJWT } from "../middlewares/auth.middleware.js";
+ import { createGroup } from "../controllers/chat.controller.js";
 const router = Router(); 
 
 
