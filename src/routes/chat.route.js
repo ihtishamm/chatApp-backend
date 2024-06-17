9ef8b72d229972430d5dac48ea7189458919ca98
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-17 20:26:07 +0500

    set the routes for the single group

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
  import { verifyJWT } from "../middlewares/auth.middleware.js";
- import { createGroup, myChat } from "../controllers/chat.controller.js";
+ import { createGroup, myChat, singleGroup } from "../controllers/chat.controller.js";
 const router = Router(); 
 
 
@@ -8,5 +8,6 @@ const router = Router();
    // authenticated routes
    router.route("/createGroup").post(verifyJWT,createGroup);
    router.route("/myChat").get(verifyJWT,myChat);
+   router.route("/group/:chatId").get(verifyJWT, singleGroup);
   
 export default router;
