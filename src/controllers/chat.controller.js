bc47d54f5e73a5822cc4f4ba2b25cbae1ca4e174
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-17 16:28:03 +0500

    write the controller for get my chat

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,15 +1,19 @@
 import { ApiError } from "../utils/ErrorHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
- import { Chat } from "../models/chat.model.js";
+import { Chat } from "../models/chat.model.js";
 import { emitEvent } from "../utils/functionns.js";
 import { ALERT, REFETCH_CHATS } from "../constants.js";
+import { getOtherUser } from "../utils/helper.js";
 
 const createGroup = asyncHandler(async (req, res) => {
   const { name, members } = req.body;
 
   if (!Array.isArray(members) || members.length < 2) {
-    throw new ApiError(400, "Group must have at least 3 members, including the creator");
+    throw new ApiError(
+      400,
+      "Group must have at least 3 members, including the creator"
+    );
   }
   const uniqueMembers = [...new Set(members)];
   if (uniqueMembers.length !== members.length) {
@@ -18,20 +22,57 @@ const createGroup = asyncHandler(async (req, res) => {
 
   // Add the current user to the members list if not already included
   const allMembers = [...new Set([...uniqueMembers, req.user])];
-//    const allMembers =[...members,req.user];
+  //    const allMembers =[...members,req.user];
 
   const group = await Chat.create({
     name,
     groupChat: true,
     creator: req.user,
-    members:allMembers
+    members: allMembers,
   });
 
-   emitEvent(req,ALERT,allMembers,`welcome to ${name} group`)
-   emitEvent(req,REFETCH_CHATS,members)
+  emitEvent(req, ALERT, allMembers, `welcome to ${name} group`);
+  emitEvent(req, REFETCH_CHATS, members);
   return res
     .status(201)
     .json(new ApiResponse(201, { group }, "Group created successfully"));
 });
 
-export { createGroup };
+const myChat = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+  const chats = await Chat.find({ members: req.user }).populate(
+    "members",
+    "fullName avatar"
+  );
+
+  const transformedChat = chats.map(({ _id, name, members, groupChat }) => {
+    const otherMember = getOtherUser(members, req.user);
+
+    return {
+      _id,
+      groupChat,
+      avatar: groupChat
+        ? members.slice(0, 3).map(({ avatar }) => avatar)
+        : [otherMember.avatar.url],
+      name: groupChat ? name : otherMember.fullName,
+      members: members.reduce((prev, curr) => {
+        if (curr._id.toString() !== req.user.toString()) {
+          prev.push(curr._id);
+        }
+        return prev;
+      }, []),
+    };
+  });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { transformedChat },
+        "User chats fetched successfully"
+      )
+    );
+});
+
+export { createGroup, myChat };
