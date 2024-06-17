faae928ef36205e21b9a30dcbf27db86b2962980
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-17 16:28:15 +0500

    write routes for getmychat

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
  import { verifyJWT } from "../middlewares/auth.middleware.js";
- import { createGroup } from "../controllers/chat.controller.js";
+ import { createGroup, myChat } from "../controllers/chat.controller.js";
 const router = Router(); 
 
 
  
    // authenticated routes
    router.route("/createGroup").post(verifyJWT,createGroup);
-
+   router.route("/myChat").get(verifyJWT,myChat);
+  
 export default router;
