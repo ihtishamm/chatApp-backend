017fc821e0c32464309bbdae4e862185f5912ee7
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-18 17:46:41 +0500

    add route for the leave group

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
  import { verifyJWT } from "../middlewares/auth.middleware.js";
- import { addMember, createGroup, myChat, removeMember, singleGroup } from "../controllers/chat.controller.js";
+ import { addMember, createGroup, leaveGroup, myChat, removeMember, singleGroup } from "../controllers/chat.controller.js";
 const router = Router(); 
 
 
@@ -8,8 +8,9 @@ const router = Router();
    // authenticated routes
    router.route("/createGroup").post(verifyJWT,createGroup);
    router.route("/myChat").get(verifyJWT,myChat);
-   router.route("/group/:chatId").get(verifyJWT, singleGroup);
+   
    router.route("/group/addMember").patch(verifyJWT, addMember);
    router.route("/group/removeMember").patch(verifyJWT, removeMember);
-   
+    router.route("/group/leave/:id").delete(verifyJWT, leaveGroup);
+   router.route("/group/:chatId").get(verifyJWT, singleGroup);
 export default router;
