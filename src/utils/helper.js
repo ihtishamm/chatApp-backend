ddb9ee3ad885f5f9be9ae5674fa6a8ae58bc1d4d
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-18 14:38:06 +0500

    add helper function to validate user

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,13 @@
+import { User } from "../models/user.model.js";
+
 export const getOtherUser = (members, userId) => {
     return members.find(member => member._id.toString() !== userId.toString())
 }
+
+export const validateUserIds = async (userIds) => {
+    const validUsers = await User.find({ '_id': { $in: userIds } }, '_id fullName');
+    if (validUsers.length !== userIds.length) {
+      throw new ApiError(400, "One or more user IDs are invalid");
+    }
+    return validUsers;
+  };
