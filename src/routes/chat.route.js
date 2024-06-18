83900328580e0b702c0f3d12615fe90ce3cba364
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-19 01:26:03 +0500

    route for get messages

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,19 +1,30 @@
 import { Router } from "express";
- import { verifyJWT } from "../middlewares/auth.middleware.js";
- import { addMember, changeGroupName, createGroup, deleteChat, getChatDetails, leaveGroup, myChat, removeMember, singleGroup } from "../controllers/chat.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
-const router = Router(); 
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  addMember,
+  changeGroupName,
+  createGroup,
+  deleteChat,
+  getChatDetails,
+  leaveGroup,
+  myChat,
+  removeMember,
+  singleGroup,
+} from "../controllers/chat.controller.js";
+const router = Router();
 
+// authenticated routes
+router.route("/createGroup").post(verifyJWT, createGroup);
+router.route("/myChat").get(verifyJWT, myChat);
 
- 
-   // authenticated routes
-   router.route("/createGroup").post(verifyJWT,createGroup);
-   router.route("/myChat").get(verifyJWT,myChat);
-   
-   router.route("/group/addMember").patch(verifyJWT, addMember);
-   router.route("/group/removeMember").patch(verifyJWT, removeMember);
-    router.route("/group/leave/:id").delete(verifyJWT, leaveGroup);
-   router.route("/group/:chatId").get(verifyJWT, singleGroup);
-    
-     router.route("/:chatId").get(verifyJWT,getChatDetails ).patch(verifyJWT,changeGroupName).delete(verifyJWT,deleteChat)
+router.route("/group/addMember").patch(verifyJWT, addMember);
+router.route("/group/removeMember").patch(verifyJWT, removeMember);
+router.route("/group/leave/:id").delete(verifyJWT, leaveGroup);
+router.route("/group/:chatId").get(verifyJWT, singleGroup);
+
+router
+  .route("/:chatId")
+  .get(verifyJWT, getChatDetails)
+  .patch(verifyJWT, changeGroupName)
+  .delete(verifyJWT, deleteChat);
 export default router;
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
  import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { sendAttachments} from "../controllers/message.controller.js";
+import { sendAttachments, getMeassage} from "../controllers/message.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router(); 
 
 router.route("/attachments").post(verifyJWT,upload.array("files",5) ,sendAttachments)
+router.route("/:id").get(verifyJWT,getMeassage)
 
 export default router;
