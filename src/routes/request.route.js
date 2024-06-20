1ec3752b0681d6ad5387d9768209e591a645651b
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-20 13:01:11 +0500

    set the routes for the recieve requests

--- src/routes/request.route.js
+++ src/routes/request.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { sendRequest } from "../controllers/request.controller.js";
+import { recieveRequest, sendRequest } from "../controllers/request.controller.js";
 
 
 const router = Router();
 
 
 router.route("/send").patch(verifyJWT, sendRequest);
+router.route("/recieve").get(verifyJWT, recieveRequest)
 
 
 export default router;
