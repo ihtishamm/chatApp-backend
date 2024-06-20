5728617dffb99494f896938aee0e061a41bd929a
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-20 12:39:06 +0500

    add the controller for the send request

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -1,9 +1,40 @@
+
+
 import { ApiError } from "../utils/ErrorHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Request } from "../models/request.model.js";
+import { emitEvent } from "../utils/functionns.js";
 
 const sendRequest = asyncHandler(async (req, res) => { 
-    res.send("Request sent");
+    const { reqId } = req.body;
+
+    if (!reqId) {
+        throw new ApiError(400, "Receiver Id is required");
+    }
+
+    // Check if a request already exists between the sender and receiver
+    const existingRequest = await Request.findOne({
+        $or: [
+            { sender: req.user._id, receiver: reqId },
+            { sender: reqId, receiver: req.user._id }
+        ]
+    });
+
+    if (existingRequest) {
+        throw new ApiError(400, "Request already sent");
+    }
+
+  
+    const request = await Request.create({
+        sender: req.user._id,
+        receiver: reqId
+    });
+
+    
+    emitEvent(req, "newRequest", [reqId], "request");
+
+    return res.status(200).json(new ApiResponse(200, request, "Request sent successfully"));
 });
 
 export { sendRequest };
