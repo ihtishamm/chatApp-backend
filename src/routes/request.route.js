ff021eb0b847aef420f6b8157e627f6a86abe2dd
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-20 13:07:33 +0500

    set route for accept request

--- src/routes/request.route.js
+++ src/routes/request.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { recieveRequest, sendRequest } from "../controllers/request.controller.js";
+import { recieveRequest, sendRequest, acceptRequest } from "../controllers/request.controller.js";
 
 
 const router = Router();
@@ -8,6 +8,7 @@ const router = Router();
 
 router.route("/send").patch(verifyJWT, sendRequest);
 router.route("/recieve").get(verifyJWT, recieveRequest)
+router.route("/accept").patch(verifyJWT, acceptRequest)
 
 
 export default router;
