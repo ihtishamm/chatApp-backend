4de4aaa0348ab20be7d28489e97879aeae7420ee
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-24 19:54:42 +0500

    create a helper function to get all the socket ids

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -11,3 +11,8 @@ export const validateUserIds = async (userIds) => {
     }
     return validUsers;
   };
+
+  export const getSockets = (users=[]) => {
+     const sockets =  users.map((user) => userSocketIds.get(user._id.toString()));
+     return sockets
+  }
